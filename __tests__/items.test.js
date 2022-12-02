ad635608b642a425a9e4b3bda22827435d473704
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-12-01 20:52:34 -0800

    update authorize middleware to handle put and delete routes

--- __tests__/items.test.js
+++ __tests__/items.test.js
@@ -3,7 +3,6 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
-const Item = require('../lib/models/Item');
 
 const mockUser = {
   email: 'email@sweet.net',
